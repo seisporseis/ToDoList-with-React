2c65a609cb22c4f8f0e3a8234d20bdecfc9682f8
Sjuniperus <sapindacea@gmail.com>
2023-05-10 12:55:02 +0200

    añadido handleSubmit, array useState y handleAddItem

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,13 +1,21 @@
 import FormTodo from "./FormTodo";
-import TaskList from "./Tasklist";
+import TaskList from "./TaskList";
 import './Container.css'
+import { useState } from "react";
 
 
 const Container = () => {
+  
+  const [list, setList] = useState([]); //creamos un array con los items
+  const handleAddItem = addItem => {
+    //setList([...list, addItem]); //esto lo cambiamos por la linea 12
+    setList(list.push(addItem));
+  };
     return (
-      <div className="container-todo-app">
-        <FormTodo/>
-        <TaskList/>
+      <div>
+
+        <FormTodo handleAddItem={handleAddItem}/>
+        <TaskList list={list} setList={setList}/>
         Container!
       </div>
     )
--- src/components/FormTodo.jsx
+++ src/components/FormTodo.jsx
@@ -1,20 +1,31 @@
 import { useState } from "react";
 
-const FormTodo = () => {
-    const [description, setDescription] = useState("")
+//esto es un componente
+const FormTodo = (props) => {
+    const [description, setDescription] = useState("");//agregaria un useState, con su descripción
+    const { handleAddItem } = props;//añade el item a la lista
+    const handleSubmit = e => { //este es el manejador del submit, cuando pulsemos el botón pasará lo que hay dentro del manejador
+        e.preventDefault();//borra el item cuando lo añadimos a la lista
+        handleAddItem ({
+            done: false, //por defecto tiene valor false y cuando se true,se marca como realizada
+            id: (+new Date()).toString(),//marca unica temporal del item.
+            description //la tarea que escribimos 
+        });
+        setDescription("");
+    };
 	return(
-        <form action="">
+        <form onSubmit={handleSubmit}>
             <div className="todo-list">
                 <div className="file-input"> 
-                    <input type="text" className="text" value={description} onChange={e=> setDescription} />
+                    <input type="text" 
+                    className="text" 
+                    value={description} 
+                    onChange={ e => setDescription(e.target.value)} />
                     <button className="btn-task" disabled={description ? "" : "disable"}>Add</button>
                 </div> 
             </div>
-           
-           
-        </form>
-        
-    )
-}
+        </form>  
+    );
+};
 
 export default FormTodo;
