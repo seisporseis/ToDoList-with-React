40bbd7009a22d2595c99fd37ed86e75b06d9ac3b
sandaliademercado <38840503+seisporseis@users.noreply.github.com>
2023-05-11 18:51:20 +0200

    css checkbox

--- src/components/CheckBox.jsx
+++ src/components/CheckBox.jsx
@@ -26,14 +26,14 @@ const Checkbox = props => {
 		<>{/*librería que agrupa elementos  */}
 		
 			<label className="todo new-item">
-			<input 
-			className ="todo__state"
-			name={id} type ="checkbox"
-			defaultChecked={done}
-			onChange={onChange}
-			/>
-			<div className="todo__text">{description}</div> 
-			<button className="btn-borrar-item" onClick={onClickBorrarItem}>borrar</button>
+				<input 
+				className ="todo__state"
+				name={id} type ="checkbox"
+				defaultChecked={done}
+				onChange={onChange}
+				/>
+				<div className="todo__text">{description}</div> 
+					<button className="btn-borrar-item" onClick={onClickBorrarItem}><img width="30" height="30" src="https://img.icons8.com/carbon-copy/100/ffffff/filled-trash.png" alt="filled-trash"/></button>
 			 {/* <button className="btn-editar-item" onClick={onClickEditarItem}>editar</button> */}
 			</label>
 			
